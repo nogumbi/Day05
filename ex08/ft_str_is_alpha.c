3107b36eaac30b58fb7a6c6a6a7e464abf0da083
Nobantu Gumbi <nogumbi@c4r18s1.wethinkcode.co.za>
2020-07-16 14:06:09 +0200

    Returns 1 if there's an alpha else 0

---
+++ ex08/ft_str_is_alpha.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nogumbi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 13:33:08 by nogumbi           #+#    #+#             */
+/*   Updated: 2020/07/16 14:05:16 by nogumbi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include<string.h>
+
+#include <unistd.h>
+#include<stdio.h>
+#include<string.h>
+
+int     ft_str_is_alpha(char *str)
+{
+    int     i;
+    i = 0;
+
+    while(str[i])
+    {
+        if((str[i] >= 'a' && str[i] <= 'z'  ) || (str[i] >= 'A' && str[i] <= 'Z'))
+            return 1;
+        else
+            return 0;
+
+    }
+    return (i);
+}
